b60420273451a91b313292078f2b19b9f5f9881b
Vijesh Shetty <vijeshsshetty@gmail.com>
2024-12-11 18:43:26 +0530

    correct

--- components/custom/Navbar.tsx
+++ components/custom/Navbar.tsx
@@ -1,89 +1,85 @@
-"use client"
+"use client";
 
 import React from "react";
 import { FloatingDock } from "@/components/ui/floating-dock";
-// import Image from "next/image";
-import { Bot, Gamepad2, Globe, Hash, House, LayoutDashboard, Phone, ShipWheel } from "lucide-react";
+import {
+  Bot,
+  Gamepad2,
+  Globe,
+  Hash,
+  House,
+  LayoutDashboard,
+  Phone,
+  ShipWheel,
+} from "lucide-react";
 
 export function Navbar() {
-	const links = [
-		{
-			title: "Home",
-			icon: (
-				<House className="h-full w-full text-[#4d1414] dark:text-neutral-300" />
-			),
-			href: '/',
-		},
+  const links = [
+    {
+      title: "Home",
+      icon: (
+        <House className="h-full w-full text-[#4d1414] dark:text-neutral-300" />
+      ),
+      href: "/",
+    },
 
-		{
-			title: "Timelines",
-			icon: (
-                <ShipWheel className="h-full w-full text-[#4d1414] dark:text-neutral-300"/>
-            ),
-			href: "/timeline",
-		},
-		{
-            title: "Social",
-			icon: (
-				<Hash className="h-full w-full text-[#4d1414] dark:text-neutral-300" />
-			),
-			href: "/social",
-		},
-		// {
-		// 	title: "Aceternity UI",
-		// 	icon: (
-		// 		<Image
-		// 			src="https://assets.aceternity.com/logo-dark.png"
-		// 			width={20}
-		// 			height={20}
-		// 			alt="Aceternity Logo"
-		// 		/>
-		// 	),
-		// 	href: "#",
-		// },
-		{
-			title: "Games",
-			icon: (
-				<Gamepad2 className="h-full w-full text-[#4d1414] dark:text-neutral-300" />
-			),
-			href: "/games",
-		},
+    {
+      title: "Timelines",
+      icon: (
+        <ShipWheel className="h-full w-full text-[#4d1414] dark:text-neutral-300" />
+      ),
+      href: "/timeline",
+    },
+    {
+      title: "Social",
+      icon: (
+        <Hash className="h-full w-full text-[#4d1414] dark:text-neutral-300" />
+      ),
+      href: "/social",
+    },
+    {
+      title: "Games",
+      icon: (
+        <Gamepad2 className="h-full w-full text-[#4d1414] dark:text-neutral-300" />
+      ),
+      href: "/games",
+    },
 
-		{
-			title: "Chat",
-			icon: (
-				<Bot className="h-full w-full text-[#4d1414] dark:text-neutral-300" />
-			),
-			href: "/chat",
-		},
-		{
-			title: "Contact",
-			icon: (
-				<Phone className="h-full w-full text-[#4d1414] dark:text-neutral-300" />
-			),
-			href: "/contact",
-		},
-		{
-			title: "Family",
-			icon: (
-				<Globe className="h-full w-full text-[#4d1414] dark:text-neutral-300"/>
-			),
-			href: "/user",
-		},
-		{
-			title: "Dashboard",
-			icon: (
-				<LayoutDashboard className="h-full w-full text-[#4d1414] dark:text-neutral-300"/>
-			),
-			href: "/dashboard",
-		},
-	];
-	return (
-		<div className=" z-10 w-full flex mt-12 justify-center items-center fixed">
-			<FloatingDock
-				mobileClassName="translate-y-20" // only for demo, remove for production
-				items={links}
-			/>
-		</div>
-	);
+    {
+      title: "Chat",
+      icon: (
+        <Bot className="h-full w-full text-[#4d1414] dark:text-neutral-300" />
+      ),
+      href: "/chat",
+    },
+    {
+      title: "Contact",
+      icon: (
+        <Phone className="h-full w-full text-[#4d1414] dark:text-neutral-300" />
+      ),
+      href: "/contact",
+    },
+    {
+      title: "Family",
+      icon: (
+        <Globe className="h-full w-full text-[#4d1414] dark:text-neutral-300" />
+      ),
+      href: "/user",
+    },
+    {
+      title: "Dashboard",
+      icon: (
+        <LayoutDashboard className="h-full w-full text-[#4d1414] dark:text-neutral-300" />
+      ),
+      href: "/dashboard",
+    },
+  ];
+  return (
+    <div className=" z-10 w-full flex mt-12 justify-center items-center fixed">
+      <FloatingDock
+        mobileClassName="translate-y-20" // only for demo, remove for production
+        items={links}
+      />
+    </div>
+  );
 }
