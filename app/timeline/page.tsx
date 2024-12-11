34309ac5ba1ed8ce3e8a31f03d010b0be8e22001
varenyathaker <varenyathaker375@gmail.com>
2024-12-11 14:59:55 +0530

    timeline revamp

--- app/timeline/page.tsx
+++ app/timeline/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const page = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen overflow-hidden">
+    <div className="flex items-center justify-center min-h-screen">
       <TimelineComponent />
     </div>
   );
