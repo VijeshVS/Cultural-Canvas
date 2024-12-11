29849e49da921feed76147713c4b41b2be52880b
varenyathaker <varenyathaker375@gmail.com>
2024-12-11 10:47:58 +0530

    timeline modified

--- app/timeline/page.tsx
+++ app/timeline/page.tsx
@@ -1,14 +1,13 @@
-"use client"
-import TimelineComponent from '@/components/custom/Timeline'
-
-import React from 'react'
+"use client";
+import TimelineComponent from '@/components/custom/Timeline';
+import React from 'react';
 
 const page = () => {
   return (
-    <div>
+    <div className="flex items-center justify-center min-h-screen overflow-hidden">
       <TimelineComponent />
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
