269ad81c371dd3482ee513fc7930f960e8e10beb
Samkit Samsukha <samkitsamsukha@gmail.com>
2025-01-22 02:49:02 +0530

    routing of create game fixed

--- app/(protected)/games/page.tsx
+++ app/(protected)/games/page.tsx
@@ -12,7 +12,7 @@ const Page = () => {
 				<button
 					className="flex flex-row space-x-2 px-6 py-2 text-[#4d1414] bg-yellow-300 rounded-full hover:scale-110 font-semibold hover:bg-yellow-400  transition-all duration-300"
 					onClick={() => {
-						router.push("/create");
+						router.push("/games/create");
 					}}
 				>
 					<Plus />
